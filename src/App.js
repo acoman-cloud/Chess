8aab8bd67bfa2e4821d1b69c355049745ec3e5cb
Anthony Coman <acoman@yahoo.com>
2022-03-02 16:21:42 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import './App.css'
+
+const initialState = [
+	[24, 25, 0, 0, 0, 0, 15, 14],
+	[23, 25, 0, 0, 0, 0, 15, 13],
+	[22, 25, 0, 0, 0, 0, 15, 12],
+	[21, 25, 0, 0, 0, 0, 15, 11],
+	[26, 25, 0, 0, 0, 0, 15, 16],
+	[22, 25, 0, 0, 0, 0, 15, 12],
+	[23, 25, 0, 0, 0, 0, 15, 13],
+	[24, 25, 0, 0, 0, 0, 15, 14],
+]
+
+function App() {
+	const [turn, setTurn] = useState('White')
+	const [board, setBoard] = useState(initialState)
+	
+	function onClick(e) {
+		let col = e.target.col;
+		let row = e.target.row;
+		//this sucks
+	}
+
+	return (
+		<div>
+		{turn ? <h1>Turn: {turn} </h1> : <h1>You Win!!</h1>}
+		<div className='board'>
+		{board.map((x, index) => {
+			return (
+				<div>
+					{x.map((subX, xIndex) => {
+						let row=`Row${xIndex+1}`
+						let diag = ''
+						if(index % 2 === 1 && xIndex % 2 === 0) diag = 'offWhite'
+						if(index % 2 === 0 && xIndex % 2 === 1) diag = 'offWhite'
+						return <h2 className={row + ' ' + diag} key={index} row={xIndex} col={index}> {
+							subX % 10 === 6 ? 'K'
+							: subX % 10 === 1 ? 'Q'
+							: subX % 10 === 2 ? 'B'
+							: subX % 10 === 3 ? 'Kn'
+							: subX % 10 === 4 ? 'R'
+							: subX % 10 === 5 ? 'P'
+							: ' '
+						} </h2>
+					})}
+				</div>
+			)
+		})}
+		</div>
+		</div>
+	);
+}
+
+export default App;
