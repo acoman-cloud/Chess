c460003b214559939e720f584698649a2295e543
Anthony Coman <acoman@yahoo.com>
2022-03-08 17:40:00 -0600

    working game

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 # Chess
-# Chess
+A working (WIP) Browser Chess game
--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Link, Switch} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 import './App.css'
 
 const initialState = [
@@ -16,39 +16,58 @@ const initialState = [
 function App() {
 	const [turn, setTurn] = useState('White')
 	const [board, setBoard] = useState(initialState)
-	
-	function onClick(e) {
-		let col = e.target.col;
-		let row = e.target.row;
-		//this sucks
-	}
+	const [selected, setSelected] = useState()
 
+	function onClick(e) {
+		let col, row
+		if (!selected) {
+			col = e.target.dataset.col;
+			row = e.target.dataset.row;
+			setSelected(board[col][row])
+			board[col].splice(row, 1, 0)
+		}
+		else if (selected) {
+			col = e.target.dataset.col;
+			row = e.target.dataset.row;
+			board[col].splice(row, 1, selected)
+			setSelected()
+			if (turn === 'White') setTurn('Black')
+			else setTurn('White')
+		}
+	}
+	
 	return (
-		<div>
-		{turn ? <h1>Turn: {turn} </h1> : <h1>You Win!!</h1>}
-		<div className='board'>
-		{board.map((x, index) => {
-			return (
-				<div>
-					{x.map((subX, xIndex) => {
-						let row=`Row${xIndex+1}`
-						let diag = ''
-						if(index % 2 === 1 && xIndex % 2 === 0) diag = 'offWhite'
-						if(index % 2 === 0 && xIndex % 2 === 1) diag = 'offWhite'
-						return <h2 className={row + ' ' + diag} key={index} row={xIndex} col={index}> {
-							subX % 10 === 6 ? 'K'
-							: subX % 10 === 1 ? 'Q'
-							: subX % 10 === 2 ? 'B'
-							: subX % 10 === 3 ? 'Kn'
-							: subX % 10 === 4 ? 'R'
-							: subX % 10 === 5 ? 'P'
-							: ' '
-						} </h2>
-					})}
-				</div>
-			)
-		})}
-		</div>
+		<div className='app'>
+			{turn ? <h1>Turn: {turn} </h1> : <h1>You Win!!</h1>}
+			<div className='board'>
+				{board.map((x, index) => {
+					return (
+						<div>
+							{x.map((subX, xIndex) => {
+								let row = `Row${xIndex + 1}`
+								let diag = '', side = ''
+								if (index % 2 === 1 && xIndex % 2 === 0) diag = 'offWhite'
+								if (index % 2 === 0 && xIndex % 2 === 1) diag = 'offWhite'
+								if (subX > 20) side = 'Black'
+								else side = 'White'
+								return <h2 className={row + ' ' + diag + ' ' + side}
+									key={index+xIndex}
+									data-col={index}
+									data-row={xIndex}
+									onClick={onClick}> {
+										subX % 10 === 6 ? 'K'
+											: subX % 10 === 1 ? 'Q'
+												: subX % 10 === 2 ? 'B'
+													: subX % 10 === 3 ? 'Kn'
+														: subX % 10 === 4 ? 'R'
+															: subX % 10 === 5 ? 'P'
+																: ' '
+									} </h2>
+							})}
+						</div>
+					)
+				})}
+			</div>
 		</div>
 	);
 }
